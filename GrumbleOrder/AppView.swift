b3d245bebfc5dc1baa572ac964b203dd0a6e3a7f
Luis <luisisthejuarez@gmail.com>
2019-09-24 16:17:14 -0500

    Added order object to environment

---
+++ GrumbleOrder/AppView.swift
@@ -0,0 +1,31 @@
+//
+//  AppView.swift
+//  GrumbleOrder
+//
+//  Created by Luis Juarez on 9/24/19.
+//  Copyright © 2019 SkyCloud. All rights reserved.
+//
+
+import SwiftUI
+
+struct AppView: View {
+    var body: some View {
+        TabView {
+            ContentView().tabItem {
+                Image(systemName: "list.dash")
+                Text("Menu")
+            }
+            OrderView().tabItem {
+                Image(systemName: "square.and.pencil")
+                Text("Order")
+            }
+        }
+    }
+}
+
+struct AppView_Previews: PreviewProvider {
+    static let order = Order()
+    static var previews: some View {
+        AppView().environmentObject(order)
+    }
+}
---
+++ GrumbleOrder/OrderView.swift
@@ -0,0 +1,43 @@
+//
+//  OrderView.swift
+//  GrumbleOrder
+//
+//  Created by Luis Juarez on 9/24/19.
+//  Copyright © 2019 SkyCloud. All rights reserved.
+//
+
+import SwiftUI
+
+struct OrderView: View {
+    @EnvironmentObject var order: Order
+    
+    var body: some View {
+        NavigationView {
+            List {
+                Section {
+                    ForEach(order.items) { item in
+                    HStack {
+                        Text(item.name)
+                        Spacer()
+                        Text("$\(item.price)")
+                        }
+                    }
+                }
+                Section {
+                    NavigationLink(destination: Text("Check Out")) {
+                        Text("Place Order")
+                    }
+                }
+            }.navigationBarTitle("Order")
+            .listStyle(GroupedListStyle())
+            
+        }
+    }
+}
+
+struct OrderView_Previews: PreviewProvider {
+    static let order = Order()
+    static var previews: some View {
+        OrderView().environmentObject(order)
+    }
+}
